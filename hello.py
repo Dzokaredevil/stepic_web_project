ee0e911f3f6f35fea18f706c50fc78006634e5f7
Dzokaredevil <dima.voytovich@mail.ru>
2018-07-02 15:45:28 +0300

    updated

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
+from cgi import parse_qs
+
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    qs = parse_qs(environ['QUERY_STRING'])
+	return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
