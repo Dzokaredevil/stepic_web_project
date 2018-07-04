aca8b3f9903c8c67a5446c89f533ec7920940121
Dzokaredevil <dima.voytovich@mail.ru>
2018-07-04 13:58:21 +0300

    updated 2.1.11

---
+++ etc/gunicorn.conf.py
@@ -0,0 +1,2 @@
+bind = '0.0.0.0:8000'
+pythonpath = '/home/box/web/ask/'
--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers)
+    body = [bytes(i + '\r\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
