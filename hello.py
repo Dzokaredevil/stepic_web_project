711ba42563f359ef9e2630a1bf40b1375eda4ef9
Dzokaredevil <dima.voytovich@mail.ru>
2018-07-04 13:40:03 +0300

    updated 2.1.11

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-def app (environ, start_response):
-	status = '200 OK'
-	response_headers = [('Content-type','text/plain')]
-	start_response(status, response_headers)
-	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-	return body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
